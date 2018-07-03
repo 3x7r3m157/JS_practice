67b504fa72c0e999f62580ccbebfa8cc8f56f0e7
Austin Atendido <austin@hackernest.com>
2018-07-03 16:34:59 -0700

    extended classes with new methods

--- ninjaclass1.js
+++ ninjaclass1.js
@@ -2,7 +2,7 @@ function ninjaConstructor(name, health = 100) {
     this.name = name
     this.health = health
     var speed = 3
-    var strength = 3
+    this.strength = 3
     this.sayName = function() {
         console.log('IT IS I, THE ONE TRUE AUSTIN!!!')
     }
@@ -15,7 +15,18 @@ function ninjaConstructor(name, health = 100) {
 }
 
 var austin = new ninjaConstructor("Austin")
+var somedude = new ninjaConstructor("Some dude")
 
-austin.showStats()
-austin.drinkSake()
-austin.showStats()
+austin.__proto__.punch = function(opponent) {
+    opponent.health -= 5
+    console.log(`${opponent.name} was punched by Austin and lost 5 health!`)
+}
+
+austin.__proto__.kick = function(opponent) {
+    kick = 15 * austin.strength
+    opponent.health -= kick
+    console.log(`${opponent.name} was kicked by Austin and lost ${kick} health!`)
+}
+
+austin.punch(somedude)
+austin.kick(somedude)
